f062f1aeb56fe16b497ed7e855a202f9604c1172
Joe Tebaldi <joetebaldi@gmail.com>
2024-11-14 13:32:01 -0500

    Refactored weather data service to add new method for reusable cache and api methods to include forecast calls

--- app/services/weather_data_service.rb
+++ app/services/weather_data_service.rb
@@ -5,41 +5,52 @@ class WeatherDataService
   # Set the base API url for 
   base_uri 'https://api.weatherapi.com/v1'  
 
-  # Fetches real-time weather data for a given zip code.
+  def initialize
+    @cache_duration = 30.minutes
+  end
+
+  # Fetches real-time weather data for a given zip code, with caching
   # Params:
-  # - zip_code (String): The zip code for which to retrieve weather data.
-  def fetch_realtime_weather(zip_code)    
-    cache_key = "realtime_weather_data_#{zip_code}"
-    
-    # Check if data is in cache
+  # - zip_code (String): The zip code for which to retrieve weather data
+  def fetch_realtime_weather(zip_code)
+    fetch_with_cache("realtime_weather_data_#{zip_code}") do
+      get_weather_data('/current.json', { q: zip_code })
+    end
+  end
+
+  # Fetches forecast weather data for a given zip code, with caching
+  # Params:
+  # - zip_code (String): The zip code for which to retrieve forecast data
+  def fetch_forecast_weather(zip_code)
+    fetch_with_cache("forecast_weather_data_#{zip_code}") do
+      get_weather_data('/forecast.json', { q: zip_code, days: 1 })
+    end
+  end
+
+  private 
+
+  # Central method to handle caching logic
+  def fetch_with_cache(cache_key)
     cached_data = Rails.cache.read(cache_key)
-    if cached_data      
-      # Return cached data with the cached flag set to true
-      return { data: cached_data, cached: true }
-    end
+    return { data: cached_data, cached: true } if cached_data
 
-    # If not cached, perform GET request to the current weather endpoint with zip code and API key
-    response = self.class.get('/current.json', query: { q: zip_code, key: ENV['WEATHER_API_KEY'] })
-    if response.success?
-      # Parse, store in cache and return the hash containing weather data
-      Rails.cache.write(cache_key, response.parsed_response, expires_in: 30.minutes)
-      # Set cached flag to false
-      { data: response.parsed_response, cached: false }
-    else
-      Rails.logger.error("Error fetching realtime weather data: #{response.message}")
-      { data: nil, cached: false }
-    end
+    # Fetch new data, store in cache, and return
+    new_api_data = yield
+    Rails.cache.write(cache_key, new_api_data, expires_in: @cache_duration)
+    { data: new_api_data, cached: false }
+  end
 
-  rescue StandardError => e
-    # Log an error if the API call fails and return nil
-    Rails.logger.error("Error fetching realtime weather data: #{e.message}")
+  # Makes a GET request to the specified weather API endpoint with the provided parameters
+  def get_weather_data(endpoint, params)
+    response = self.class.get(endpoint, query: params.merge(key: ENV['WEATHER_API_KEY']))
+    # Parse, store in cache if success, log error if error
+    response.success? ? response.parsed_response : log_error("Error fetching weather data: #{response.message}")
+  end
+
+  # Logs errors encountered during API calls
+  def log_error(message)
+    Rails.logger.error(message)
     nil
   end
 
-  # Fetches forecast weather data for a given zip code.
-  # Params:
-  # - zip_code (String): The zip code for which to retrieve forecast data.
-  def fetch_forecast_weather(zip_code)
-    # TODO - Implement forecast fetching logic here
-  end
 end

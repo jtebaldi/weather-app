ca9b5a31c4d63fa9eaf4afc9daffe738a126d13a
Joe Tebaldi <joetebaldi@gmail.com>
2024-11-14 13:48:43 -0500

    Updated weather controller and view to display min/max temps

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -8,12 +8,11 @@ class WeatherController < ApplicationController
   def show
     # Initialize the WeatherDataService, which handles communication with the weather API
     weather_data_service = WeatherDataService.new
-    # Fetch real-time weather data for the zip code provided in params
-    # Returns a hash containing :data (api weather data) and :cached (boolean)
-    realtime_data_result = weather_data_service.fetch_realtime_weather(params[:zip_code])
-
-    # Make the weather data and cache status accessible in the view
-    @realtime_weather_data = realtime_data_result[:data]
-    @cached = realtime_data_result[:cached]
+    
+    # Fetch real-time and forecast weather data for the zip code provided in params
+    # Returns a hash containing :data (api realtime weather data) and :cached (boolean)    
+    weather_data_result = weather_data_service.fetch_weather_data(params[:zip_code])
+    @weather_data = weather_data_result[:data]
+    @cached = weather_data_result[:cached]
   end
 end
